6e74f0a01b0f4aecaf848ff229a8a7668adee7c6
Steve Speicher <steve.speicher@hashicorp.com>
2025-09-18 10:52:00 -0400

    smaller AAP samples

---
+++ aap/main-aap-app-server.tf
@@ -0,0 +1,35 @@
+
+# Provision the AWS EC2 instance(s)
+resource "aws_instance" "web_server" {
+  count                     = 5
+  ami                       = "ami-0dfc569a8686b9320" # Red Hat Enterprise Linux
+  instance_type             = "t2.micro"
+  key_name                  = var.ssh_key_name
+  vpc_security_group_ids    = [aws_security_group.allow_http_ssh.id]
+  associate_public_ip_address = true
+  tags = {
+    Name = "hcp-terraform-aap-demo-${count.index + 1}"
+  }  
+  lifecycle {
+    action_trigger {
+      events  = [after_create]
+      actions = [action.aap_eventdispatch.configure]
+    }
+  }
+}
+
+# TF action to run the new AWS provisioning workflow (after ec2 instance are created)
+action "aap_eventdispatch" "configure" {
+  config {
+    limit = "tfademo"
+    template_type = "job"
+    job_template_name = "New AWS Provisioning Workflow"
+    organization_name = "Default"
+
+    event_stream_config = {
+      url = var.aap_eventstream_url
+      username = var.tf-es-username
+      password = var.tf-es-password
+    }
+  }
+}
---
+++ aap/main-aap-inventory.tf
@@ -0,0 +1,35 @@
+# Add the new EC2 instance to the inventory
+resource "aap_host" "host" {
+  for_each     = { for idx, instance in aws_instance.web_server : idx => instance }
+  inventory_id = data.aap_inventory.inventory.id
+  groups = toset([resource.aap_group.tfademo.id])
+  name         = each.value.public_ip
+  description  = "Host provisioned by Terraform"
+  variables    = jsonencode({
+    ansible_user = "ec2-user"
+    public_ip    = each.value.public_ip
+    target_hosts = each.value.public_ip
+  })
+  lifecycle {
+    action_trigger {
+      events  = [after_create]
+      actions = [action.aap_eventdispatch.update]
+    }
+  }
+}
+
+# TF action to run the update AWS provisioning job (after the hosts get added to AAP inventory)
+action "aap_eventdispatch" "update" {
+  config {
+    limit = "tfademo"
+    template_type = "job"
+    job_template_name = "Update AWS Provisioning Job"
+    organization_name = "Default"
+
+    event_stream_config = {
+      url = var.aap_eventstream_url
+      username = var.tf-es-username
+      password = var.tf-es-password
+    }
+  }
+}
