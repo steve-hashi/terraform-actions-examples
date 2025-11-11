5489b9474270999726544be87d19264ba603f4a0
Steve Speicher <steve.speicher@hashicorp.com>
2025-11-11 15:01:41 -0500

    a working Azure sample

--- azurerm/main.tf
+++ azurerm/main.tf
@@ -1,27 +1,74 @@
 terraform {
-  required_version = "~> v1.14.0"
+//  required_version = "~> v1.14.0"
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "4.45.1"
+    }
+  }
+  cloud { 
+    organization = "team-tf-actions-test-org" 
+    workspaces { 
+      name = "steve-azurerm-demo" 
+    } 
+  } 
+}
+
+provider "azurerm" {
+  features {
+  virtual_machine {
+      detach_implicit_data_disk_on_deletion = false
+      delete_os_disk_on_deletion            = true
+      skip_shutdown_and_force_delete        = false
     }
   }
 }
 
+resource "azurerm_resource_group" "test" {
+  name     = "steve-demo"
+  location = "eastus"
+}
+
+resource "azurerm_virtual_network" "test" {
+  name                = "steve-demo"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+}
+
+resource "azurerm_subnet" "test" {
+  name                 = "steve-demo"
+  resource_group_name  = azurerm_resource_group.test.name
+  virtual_network_name = azurerm_virtual_network.test.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "example"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = "eastus"
+  ip_configuration {
+    name                          = "steve-demo"
+    subnet_id                     = azurerm_subnet.test.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
 
 resource "azurerm_linux_virtual_machine" "example"  {
   name                = "example-machine"
-  resource_group_name = "ex-rg"
-  location            = "ex-loc"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = "eastus"
   size                = "Standard_F2"
   admin_username      = "adminuser"
+  admin_password      = "P@ssw0rd1234!"
   network_interface_ids = [
     azurerm_network_interface.example.id,
   ]
-  admin_ssh_key {
+  disable_password_authentication = false
+
+/*  admin_ssh_key {
     username   = "adminuser"
     public_key = file("~/.ssh/id_rsa.pub")
-  }
+  } */
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
@@ -32,30 +79,29 @@ resource "azurerm_linux_virtual_machine" "example"  {
     sku       = "18.04-LTS"
     version   = "latest"
   }
-}
-
-resource "azurerm_network_interface" "example" {
-  name                = "example"
-  resource_group_name = "ex-rg"
-  location            = "ex-loc"
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = "sn-id"
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "terraform_data" "example" {
-  input = azurerm_network_interface.example.private_ip_address
   lifecycle {
     action_trigger {
-      events  = [after_update]
-      actions = [action.azurerm_virtual_machine_power.example]
+      events  = [after_create]
+      actions = [action.azurerm_virtual_machine_power.stop]
     }
   }
 }
 
-action "azurerm_virtual_machine_power" "example" {
+action "azurerm_virtual_machine_power" "stop" {
+  config {
+    virtual_machine_id = azurerm_linux_virtual_machine.example.id
+    power_action       = "power_off"
+  }
+}
+
+action "azurerm_virtual_machine_power" "start" {
+  config {
+    virtual_machine_id = azurerm_linux_virtual_machine.example.id
+    power_action       = "power_on"
+  }
+}
+
+action "azurerm_virtual_machine_power" "restart" {
   config {
     virtual_machine_id = azurerm_linux_virtual_machine.example.id
     power_action       = "restart"
